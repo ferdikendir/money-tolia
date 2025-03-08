a60b1d4bf1614e694aa31b39221515a4cb237acf
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-03-08 12:17:58 +0300

    (feature/Campaign) - Added campaign list component

--- src/app/campaign/campaign.component.ts
+++ src/app/campaign/campaign.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { Campaign, CampaignService } from '@core/api';
+import { MomentFormatPipe } from '../shared/pipes/moment.pipe';
 
 @Component({
   selector: 'money-tolia-campaign',
-  imports: [SharedModule],
+  imports: [SharedModule, MomentFormatPipe],
   templateUrl: './campaign.component.html',
   styleUrl: './campaign.component.scss'
 })
 export class CampaignComponent {
 
-  constructor(private toastr: ToastrService) {
-    this.toastr.success('Hello world!', 'Toastr fun!');
+  private readonly campaignService = inject(CampaignService);
+
+  campaigns: Campaign[] = [];
+
+  constructor() {
+
+    this.campaignService.campaigns$.subscribe(campaigns => this.campaigns = campaigns);
   }
 
 }
