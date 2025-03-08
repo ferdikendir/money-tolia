e2210697378a052ce31c4c55245724b4f6c43826
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-03-08 18:22:14 +0300

    (feature/Responsive) - Fixed responsive

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from "../../shared/shared.module";
+import { SideMenuComponent } from 'src/app/shared/components/side-menu/side-menu.component';
+import { LayoutService } from 'src/app/shared/services/layout.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'money-tolia-header',
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+    SideMenuComponent,
+    AsyncPipe
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  private readonly layoutService = inject(LayoutService);
+
+  ltMd$ = this.layoutService.ltMd$;
 
 }
--- src/app/layouts/layout/layout.component.ts
+++ src/app/layouts/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { LayoutService } from 'src/app/shared/services/layout.service';
 
 @Component({
   selector: 'money-tolia-layout',
@@ -14,5 +15,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+
+  private readonly layoutService = inject(LayoutService);
+
+  private screenSize = false;
+
+  @HostBinding('style.--visible-sidenav') get visible(): string {
+    return this.screenSize ? 'none' : 'unset';
+  }
+
+  @HostBinding('style.--sidenav-container-width') get width(): string {
+    return this.screenSize ? '0px' : '280px';
+  }
+
+  constructor() {
+    this.layoutService.ltMd$.subscribe(response => {
+      this.screenSize = response;
+    })
+  }
 
 }
