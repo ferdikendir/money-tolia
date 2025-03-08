1eec366be084799a8b1b58be2ea9f0ed1abe57c4
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-03-08 12:18:16 +0300

    (feature/Campaign) - Added campaign add component

--- src/app/campaign/campaign-add/campaign-add.component.ts
+++ src/app/campaign/campaign-add/campaign-add.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+import moment from 'moment';
+import { ToastrService } from 'ngx-toastr';
+import { Campaign, CampaignService } from '@core/api';
 @Component({
   selector: 'money-tolia-campaign-add',
-  imports: [],
+  imports: [
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './campaign-add.component.html',
   styleUrl: './campaign-add.component.scss'
 })
 export class CampaignAddComponent {
+
+  private readonly toastr = inject(ToastrService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly campaignService = inject(CampaignService);
+
+  campaignForm = this.formBuilder.group({
+    header: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    score: [0],
+    campaignDate: [moment()],
+  });
+
+  constructor() {
+  }
+
+  addCampaign() {
+
+    this.campaignForm.markAllAsTouched();
+
+    if (this.campaignForm.invalid) {
+      return;
+    }
+
+    this.toastr.success('The campaign has been added successfully.', 'Campaign Added');
+    this.campaignService.addCampaign(this.campaignForm.value as Campaign);
+  }
 
 }
